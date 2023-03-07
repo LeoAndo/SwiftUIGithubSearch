05ec13c2a52d8aa1dfe8c18a916a8c796c0abd5f
ando <leo81@outlook.jp>
2023-03-07 15:04:50 +0900

    init project

---
+++ SwiftUIGithubSearch/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIGithubSearch
+//
+//  Created by LeoAndo on 2023/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
